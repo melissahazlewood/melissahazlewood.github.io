b5decac69e738379379275b3841effe01d77f12c
Melissa Hazlewood <meli.hazlewood@gmail.com>
2021-08-05 14:56:26 -0700

    Contact form first iteration (test)

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$emailFrom = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+
+	$emailTo = "coolsis6@yahoo.com";
+	$headers = "From: ".$emailFrom." via my website\n";
+	$txt = "You have received an email from ".$name.".\n\n".$message;
+
+	mail($emailTo, $subject, $txt, $headers);
+	header("Location: contact.html?mailsend");
+
+}
